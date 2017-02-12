056d81fa0fb88e5f657df5800b5c4aeb5e497637
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 14:03:29 -0500

    Add project files.

---
+++ ProjectIO/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ProjectIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListFiles = new System.Windows.Forms.Button();
+            this.btnListSub = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.rtbNoteDisplay = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnListFiles
+            // 
+            this.btnListFiles.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListFiles.Location = new System.Drawing.Point(408, 32);
+            this.btnListFiles.Name = "btnListFiles";
+            this.btnListFiles.Size = new System.Drawing.Size(190, 55);
+            this.btnListFiles.TabIndex = 0;
+            this.btnListFiles.Text = "List Files";
+            this.btnListFiles.UseVisualStyleBackColor = true;
+            this.btnListFiles.Click += new System.EventHandler(this.btnListFiles_Click);
+            // 
+            // btnListSub
+            // 
+            this.btnListSub.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListSub.Location = new System.Drawing.Point(212, 32);
+            this.btnListSub.Name = "btnListSub";
+            this.btnListSub.Size = new System.Drawing.Size(190, 55);
+            this.btnListSub.TabIndex = 1;
+            this.btnListSub.Text = "List Sub";
+            this.btnListSub.UseVisualStyleBackColor = true;
+            this.btnListSub.Click += new System.EventHandler(this.btnListSub_Click);
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 32);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(190, 55);
+            this.btnSelectFolder.TabIndex = 2;
+            this.btnSelectFolder.Text = "Select a Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRename.Location = new System.Drawing.Point(477, 111);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(120, 60);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(330, 111);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(120, 60);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnMove
+            // 
+            this.btnMove.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMove.Location = new System.Drawing.Point(170, 111);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(120, 60);
+            this.btnMove.TabIndex = 5;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(12, 111);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(120, 60);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(12, 184);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(147, 23);
+            this.lblDescription.TabIndex = 7;
+            this.lblDescription.Text = "Folder: C:\\Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Note";
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            this.lstDisplay.ItemHeight = 15;
+            this.lstDisplay.Location = new System.Drawing.Point(12, 210);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(579, 109);
+            this.lstDisplay.TabIndex = 9;
+            // 
+            // rtbNoteDisplay
+            // 
+            this.rtbNoteDisplay.Location = new System.Drawing.Point(12, 348);
+            this.rtbNoteDisplay.Name = "rtbNoteDisplay";
+            this.rtbNoteDisplay.Size = new System.Drawing.Size(579, 110);
+            this.rtbNoteDisplay.TabIndex = 10;
+            this.rtbNoteDisplay.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 474);
+            this.Controls.Add(this.rtbNoteDisplay);
+            this.Controls.Add(this.lstDisplay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnListSub);
+            this.Controls.Add(this.btnListFiles);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListFiles;
+        private System.Windows.Forms.Button btnListSub;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstDisplay;
+        private System.Windows.Forms.RichTextBox rtbNoteDisplay;
+    }
+}
+
---
+++ ProjectIO/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+namespace ProjectIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string sourceFolderPath = @"T:\COMP";
+        string targetFolderPath = @"U:\TEST";
+
+        public void RefreshListBox(ListBox lb, string[] sa)
+        {
+            lb.Items.Clear();
+            lb.Items.AddRange(sa);//AddRange is to add the whole array
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diaglog = new FolderBrowserDialog();
+            diaglog.RootFolder = Environment.SpecialFolder.MyComputer;
+            diaglog.SelectedPath = sourceFolderPath;
+            diaglog.Description = "Please select a folder.";
+            diaglog.ShowNewFolderButton = true; //allows creating a new folder
+            DialogResult result = diaglog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                sourceFolderPath = diaglog.SelectedPath;
+                lblDescription.Text = "Folder: " + sourceFolderPath;
+                rtbNoteDisplay.Text = "You have selected " + sourceFolderPath +
+                    " as source folder.";
+            }
+            else
+            {
+                MessageBox.Show("operation canceled by user");
+                return;
+            }
+
+        }
+
+        private void btnListSub_Click(object sender, EventArgs e)
+        {
+            lstDisplay.Items.Clear(); //clear the listbox
+            //all subfolder paths will be stored in subfolderPaths array
+            String[] subFolderPaths = Directory.GetDirectories(sourceFolderPath);
+            //Now display all subfolder paths in ListBox
+            if (subFolderPaths.Length > 0)
+            {
+                RefreshListBox(lstDisplay, subFolderPaths);
+                rtbNoteDisplay.Text = "Subfolders of " + sourceFolderPath + " are listed now.";
+            }
+            else
+            {
+                rtbNoteDisplay.Text = sourceFolderPath + " doesn't have subfolders.";
+            }
+        }
+
+        private void btnListFiles_Click(object sender, EventArgs e)
+        {
+            lstDisplay.Items.Clear(); //clear the listbox
+            //all file paths will be stored in filePaths array
+            String[] filePaths = Directory.GetFiles(sourceFolderPath);
+            //now display all paths in ListBox lstDisplay
+            if (filePaths.Length > 0)
+            {
+                RefreshListBox(lstDisplay, filePaths);
+                rtbNoteDisplay.Text = "Files in " + sourceFolderPath + " are listed now.";
+            } 
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if ((lstDisplay.SelectedIndex != -1))
+            {
+                string filePath = lstDisplay.SelectedItem.ToString();
+                //target file path should be a full path, not just the folder path
+                string targetFilePath = filePath.Replace(sourceFolderPath, targetFolderPath);
+                try
+                {
+                    File.Copy(filePath, targetFilePath, true);
+                    rtbNoteDisplay.Text = filePath + " is copied to " + targetFolderPath;
+                }
+                catch (Exception ex)
+                {
+                    rtbNoteDisplay.Text = ex.Message;
+                }
+            }
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if ((lstDisplay.SelectedIndex != -1))
+            {
+                string filePath = lstDisplay.SelectedItem.ToString();
+                string newFilePath = filePath.Replace(targetFolderPath, targetFolderPath);
+                try
+                {
+                    File.Move(filePath, newFilePath);
+                    //refresh the ListBox is required as one file has been moved out
+                    btnListFiles.PerformClick();
+                    rtbNoteDisplay.Text = filePath + " is moved to " + targetFolderPath;
+                }
+                catch (Exception ex)
+                {
+                    rtbNoteDisplay.Text = ex.Message;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if ((lstDisplay.SelectedIndex != -1))
+            {
+                string filePath = lstDisplay.SelectedItem.ToString();
+                try
+                {
+                    File.Delete(filePath);
+                    rtbNoteDisplay.Text = filePath + " is deleted.";
+                    btnListFiles.PerformClick();
+                }
+                catch (Exception ex)
+                {
+                    rtbNoteDisplay.Text = ex.Message;
+                }
+            }
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if ((lstDisplay.SelectedIndex != -1))
+            {
+                string filePath = lstDisplay.SelectedItem.ToString();
+                //get the index of the last \
+                int lastSlash = filePath.LastIndexOf('\\');
+                string currentFileName = filePath.Substring(lastSlash + 1);
+                //add MS VisualBasic reference for InputBox
+                string newFileName = Interaction.InputBox(
+                    "Current file name is " + currentFileName + @"
+Please type new file name.");
+                string newFilePath = Path.Combine(sourceFolderPath, newFileName);
+                try
+                {
+                    File.Move(filePath, newFilePath);
+                    btnListFiles.PerformClick();
+                    rtbNoteDisplay.Text = currentFileName + " is renamed " + newFileName;
+                }
+                catch (Exception ex)
+                {
+                    rtbNoteDisplay.Text = ex.Message;
+                }
+            }
+        }
+    }
+}
